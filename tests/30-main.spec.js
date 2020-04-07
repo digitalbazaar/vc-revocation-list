bafe6f991a7f2e1b7e133a1f02cb1a08960c636d
Dave Longley <dlongley@digitalbazaar.com>
2020-04-07 19:26:46 -0400

    Add more `main` tests.

--- tests/30-main.spec.js
+++ tests/30-main.spec.js
@@ -1,10 +1,78 @@
 /*!
  * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
  */
-import {createList, decodeList, createCredential} from '..';
+import {createList, decodeList, createCredential, checkStatus} from '..';
+import {extendContextLoader} from 'jsonld-signatures';
+import vc from 'vc-js';
+
+const {defaultDocumentLoader} = vc;
 
 const encodedList100k =
   'H4sIAAAAAAAAA-3BMQEAAADCoPVPbQsvoAAAAAAAAAAAAAAAAP4GcwM92tQwAAA';
+const encodedList100KWith50KthRevoked =
+  'H4sIAAAAAAAAA-3OMQ0AAAgDsOHfNB72EJJWQRMAAAAAAIDWXAcAAAAAAIDHFrc4zDz' +
+  'UMAAA';
+
+const documents = new Map();
+documents.set('https://w3id.org/vc-revocation-list/v1', {
+  '@context': {
+    '@protected': true,
+    RevocationList2020: {
+      '@id': 'https://w3id.org/vc-revocation-list#RevocationList2020',
+      '@type': '@id',
+      '@context': {
+        '@protected': true,
+        encodedList: 'https://w3id.org/vc-revocation-list#encodedList'
+      }
+    },
+    RevocationList2020Status: {
+      '@id': 'https://w3id.org/vc-revocation-list#RevocationList2020Status',
+      '@type': '@id',
+      '@context': {
+        '@protected': true,
+        revocationListIndex: {
+          '@id': 'https://w3id.org/vc-revocation-list#revocationListIndex',
+          '@type': 'http://www.w3.org/2001/XMLSchema#integer'
+        },
+        revocationListCredential: {
+          '@id': 'https://w3id.org/vc-revocation-list#revocationListIndex',
+          '@type': '@id'
+        }
+      }
+    },
+    RevocationList2020Credential: {
+      '@id': 'https://w3id.org/vc-revocation-list#RevocationList2020Credential',
+      '@type': '@id'
+    }
+  }
+});
+
+const RLC = {
+  '@context': [
+    'https://www.w3.org/2018/credentials/v1',
+    'https://w3id.org/vc-revocation-list/v1'
+  ],
+  id: 'https://example.com/status/1',
+  type: ['VerifiableCredential', 'RevocationList2020Credential'],
+  credentialSubject: {
+    id: `https://example.com/status/1#list`,
+    type: 'RevocationList2020',
+    encodedList: encodedList100KWith50KthRevoked
+  }
+};
+documents.set(RLC.id, RLC);
+
+const documentLoader = extendContextLoader(async url => {
+  const doc = documents.get(url);
+  if(doc) {
+    return {
+      contextUrl: null,
+      documentUrl: url,
+      document: doc
+    };
+  }
+  return defaultDocumentLoader(url);
+});
 
 describe('main', () => {
   it('should create a list', async () => {
@@ -46,5 +114,123 @@ describe('main', () => {
         encodedList: encodedList100k
       }
     });
+  });
+
+  it('should verify the status of a credential', async () => {
+    const credential = {
+      '@context': [
+        'https://www.w3.org/2018/credentials/v1',
+        'https://w3id.org/vc-revocation-list/v1'
+      ],
+      id: 'urn:uuid:a0418a78-7924-11ea-8a23-10bf48838a41',
+      type: ['VerifiableCredential', 'example:TestCredential'],
+      credentialSubject: {
+        id: 'urn:uuid:4886029a-7925-11ea-9274-10bf48838a41',
+        'example:test': 'foo'
+      },
+      credentialStatus: {
+        id: 'https://example.com/status/1#67342',
+        type: 'RevocationList2020Status',
+        revocationListIndex: '67342',
+        revocationListCredential: RLC.id
+      }
+    };
+    const result = await checkStatus({
+      credential, documentLoader, verifyRevocationListCredential: false});
+    result.verified.should.equal(true);
+  });
+
+  it('should fail to verify status with incorrect status type', async () => {
+    const credential = {
+      '@context': [
+        'https://www.w3.org/2018/credentials/v1',
+        'https://w3id.org/vc-revocation-list/v1'
+      ],
+      id: 'urn:uuid:a0418a78-7924-11ea-8a23-10bf48838a41',
+      type: ['VerifiableCredential', 'example:TestCredential'],
+      credentialSubject: {
+        id: 'urn:uuid:4886029a-7925-11ea-9274-10bf48838a41',
+        'example:test': 'foo'
+      },
+      credentialStatus: {
+        id: 'https://example.com/status/1#67342',
+        type: 'ex:NonmatchingStatusType',
+        revocationListIndex: '67342',
+        revocationListCredential: RLC.id
+      }
+    };
+    const result = await checkStatus({
+      credential, documentLoader, verifyRevocationListCredential: false});
+    result.verified.should.equal(false);
+  });
+
+  it('should fail to verify status with missing index', async () => {
+    const credential = {
+      '@context': [
+        'https://www.w3.org/2018/credentials/v1',
+        'https://w3id.org/vc-revocation-list/v1'
+      ],
+      id: 'urn:uuid:a0418a78-7924-11ea-8a23-10bf48838a41',
+      type: ['VerifiableCredential', 'example:TestCredential'],
+      credentialSubject: {
+        id: 'urn:uuid:4886029a-7925-11ea-9274-10bf48838a41',
+        'example:test': 'foo'
+      },
+      credentialStatus: {
+        id: 'https://example.com/status/1#67342',
+        type: 'RevocationList2020Status',
+        revocationListCredential: RLC.id
+      }
+    };
+    const result = await checkStatus({
+      credential, documentLoader, verifyRevocationListCredential: false});
+    result.verified.should.equal(false);
+  });
+
+  it('should fail to verify status with missing list credential', async () => {
+    const credential = {
+      '@context': [
+        'https://www.w3.org/2018/credentials/v1',
+        'https://w3id.org/vc-revocation-list/v1'
+      ],
+      id: 'urn:uuid:a0418a78-7924-11ea-8a23-10bf48838a41',
+      type: ['VerifiableCredential', 'example:TestCredential'],
+      credentialSubject: {
+        id: 'urn:uuid:4886029a-7925-11ea-9274-10bf48838a41',
+        'example:test': 'foo'
+      },
+      credentialStatus: {
+        id: 'https://example.com/status/1#67342',
+        type: 'RevocationList2020Status',
+        revocationListIndex: '67342'
+      }
+    };
+    const result = await checkStatus({
+      credential, documentLoader, verifyRevocationListCredential: false});
+    result.verified.should.equal(false);
+  });
+
+  it('should fail to verify status for revoked credential', async () => {
+    const credential = {
+      '@context': [
+        'https://www.w3.org/2018/credentials/v1',
+        'https://w3id.org/vc-revocation-list/v1'
+      ],
+      id: 'urn:uuid:e74fb1d6-7926-11ea-8e11-10bf48838a41',
+      type: ['VerifiableCredential', 'example:TestCredential'],
+      credentialSubject: {
+        id: 'urn:uuid:011e064e-7927-11ea-8975-10bf48838a41',
+        'example:test': 'bar'
+      },
+      credentialStatus: {
+        id: 'https://example.com/status/1#50000',
+        type: 'RevocationList2020Status',
+        revocationListIndex: '50000',
+        revocationListCredential: RLC.id
+      }
+    };
+    const result = await checkStatus({
+      credential, documentLoader, verifyRevocationListCredential: false});
+    result.verified.should.equal(false);
   });
 });
