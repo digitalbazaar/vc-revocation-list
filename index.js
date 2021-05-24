a6e60bdade8ff7d9d60859463f4db17d658e0bc2
Matthew Collier <mcollier@digitalbazaar.com>
2021-05-24 13:02:19 -0400

    Update copyright notices.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2020-2021 Digital Bazaar, Inc. All rights reserved.
  */
 // translate `main.js` to CommonJS
 require = require('esm')(module);
